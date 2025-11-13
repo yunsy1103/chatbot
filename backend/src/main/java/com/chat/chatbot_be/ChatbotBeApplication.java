a332a5aa06a813b356f6cc9956d219b865998e13
yunsy1103 <yunsy1103@naver.com>
2025-11-13 11:10:49 +0900

    Init backend project

---
+++ backend/src/main/java/com/chat/chatbot_be/ChatbotBeApplication.java
@@ -0,0 +1,13 @@
+package com.chat.chatbot_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotBeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/chat/chatbot_be/ChatbotBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.chatbot_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
